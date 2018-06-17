094c032afa9d5bfad30f6cf17b2c8af2ccea92cf
pradhisha <pradhisharajendran@gmail.com>
2018-06-16 23:36:19 -0500

    hi

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import random
 import urllib
 import datetime
 import json
+import redis
 
 app = Flask(__name__)
 
 
-server = 'sham05.database.windows.net'
-database = 'sqldb'
-username = 'sham05'
-password = '1qaz!QAZ'
+server = 'cloudazure.database.windows.net'
+database = 'sqldatabase'
+username = 'Pradhi'
+password = 'qwerty@123'
 driver= '{ODBC Driver 13 for SQL Server}'
    
 def disdata():
@@ -26,18 +27,6 @@ def disdata():
    executiontime = end - start
    return render_template('searchearth.html', ci=row, t=executiontime)
 
-def randrange(rangfro=None,rangto=None,num=None):
-    dbconn = pypyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = dbconn.cursor()
-    start = time.time()
-    for i in range(0,int(num)):
-    	mag= round(random.uniform(rangfro, rangto),2)
-    	success="SELECT * from [earth_data] where mag>'"+str(mag)+"'"
-    	cursor.execute(success)
-    end = time.time()
-    exectime = end - start
-    return render_template('count.html', t=exectime)
-
 @app.route('/')
 def hello_world():
   return render_template('index.html')
@@ -45,13 +34,6 @@ def hello_world():
 @app.route('/displaydata', methods=['POST'])
 def display():
     return disdata() 
-
-@app.route('/multiplerun', methods=['GET'])
-def randquery():
-    rangfro = float(request.args.get('rangefrom'))
-    rangto = float(request.args.get('rangeto'))
-    num = request.args.get('nom')
-    return randrange(rangfro,rangto,num) 	
 
 if __name__ == '__main__':
   app.run()
